d465e9278e4b325a01ec5b567c8b11d9ac9fa76f
Szilard Parrag <szilard.parrag@gmail.com>
2023-03-24 12:56:08 +0100

    nostr: no_std support

--- crates/nostr/src/event/unsigned.rs
+++ crates/nostr/src/event/unsigned.rs
@@ -3,6 +3,9 @@
 
 //! Unsigned Event
 
+#[cfg(feature = "alloc")]
+use alloc::{string::{String, ToString}, vec::Vec};
+
 use secp256k1::schnorr::Signature;
 use secp256k1::{Message, XOnlyPublicKey};
 use serde::{Deserialize, Serialize};
