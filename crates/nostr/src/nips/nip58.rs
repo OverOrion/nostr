a9381892c7d92e7ed050a82d081517d7c2aa112c
Szilard Parrag <szilard.parrag@gmail.com>
2023-05-02 14:55:24 +0200

    nostr/nips: add test for Event Definition (NIP58), fix image tag with dimensions

--- crates/nostr/src/nips/nip58.rs
+++ crates/nostr/src/nips/nip58.rs
@@ -85,9 +85,9 @@ impl BadgeDefinitionBuilder {
         if let Some(image) = self.image {
             let image_tag = if let Some(width_height) = self.image_dimensions {
                 let ImageDimensions(width, height) = width_height;
-                Tag::ImageWithDimensions(image, width, height)
+                Tag::Image(image, Some((width, height)))
             } else {
-                Tag::Image(image)
+                Tag::Image(image, None)
             };
             tags.push(image_tag);
         }
@@ -101,9 +101,9 @@ impl BadgeDefinitionBuilder {
                 let thumb_url = thumb.0;
                 let thumb_tag = if let Some(width_height) = thumb.1 {
                     let ImageDimensions(width, height) = width_height;
-                    Tag::ThumbWithDimensions(thumb_url, width, height)
+                    Tag::Thumb(thumb_url, Some((width, height)))
                 } else {
-                    Tag::Thumb(thumb_url)
+                    Tag::Thumb(thumb_url, None)
                 };
                 tags.push(thumb_tag);
             }
@@ -234,5 +234,37 @@ impl ProfileBadgesEvent {
         let event = event_builder.to_event(keys)?;
 
         Ok(ProfileBadgesEvent(event))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_badge_definition_builder() {
+        let example_event_json = r#"{"content":"","id":"378f145897eea948952674269945e88612420db35791784abf0616b4fed56ef7","sig":"fd0954de564cae9923c2d8ee9ab2bf35bc19757f8e328a978958a2fcc950eaba0754148a203adec29b7b64080d0cf5a32bebedd768ea6eb421a6b751bb4584a8","created_at":1671739153,"pubkey":"79dff8f82963424e0bb02708a22e44b4980893e3a4be0fa3cb60a43b946764e3","kind":30009,"tags":[["d","bravery"],["name","Medal of Bravery"],["image","https://nostr.academy/awards/bravery.png","1024x1024"],["description","Awarded to users demonstrating bravery"],["thumb","https://nostr.academy/awards/bravery_256x256.png","256x256"]]}"#;
+
+        let example_event: Event = serde_json::from_str(example_event_json).unwrap();
+
+        let mut builder = BadgeDefinitionBuilder::new("bravery".to_owned());
+        let image_dimensions = ImageDimensions(1024, 1024);
+        let thumb_size = ImageDimensions(256, 256);
+        let thumbs = vec![(
+            "https://nostr.academy/awards/bravery_256x256.png".to_owned(),
+            Some(thumb_size),
+        )];
+        builder = builder
+            .name("Medal of Bravery".to_owned())
+            .description("Awarded to users demonstrating bravery".to_owned())
+            .image("https://nostr.academy/awards/bravery.png".to_owned())
+            .image_dimensions(image_dimensions)
+            .thumbs(thumbs);
+
+        let keys = Keys::generate();
+        let badge_definition_event = builder.build(&keys).unwrap().0;
+
+        assert_eq!(badge_definition_event.kind, Kind::BadgeDefinition);
+        assert_eq!(badge_definition_event.tags, example_event.tags);
     }
 }
